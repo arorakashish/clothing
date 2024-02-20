1af2f4aaadfd7535796b3b0a6037bcb3d8b85739
arorakashish <50692447+arorakashish@users.noreply.github.com>
2024-02-20 11:43:33 +0530

    Optimize created directory component - everything working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import CategoryItem from './components/category-item/category-item.component';
-import './categories.stye.scss';
+import './components/directory/directory.styles.scss';
+import Directory from './components/directory/directory.componet';
 
 const App = () => {
   const categories = [
@@ -30,13 +30,7 @@ const App = () => {
     },
   ];
 
-  return (
-    <div className="categories-container">
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
-      ))}
-    </div>
-  );
+  return <Directory categories={categories} />;
 };
 
 export default App;
