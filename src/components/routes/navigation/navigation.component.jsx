48d52e4a192a03aa8be5ee15067959df15219cc5
arorakashish <50692447+arorakashish@users.noreply.github.com>
2024-02-20 14:54:41 +0530

    Nav, router and firebase install

---
+++ src/components/routes/navigation/navigation.component.jsx
@@ -0,0 +1,24 @@
+import { Outlet, Link } from 'react-router-dom';
+import { Fragment } from 'react';
+import './navigation.styles.scss';
+import { ReactComponent as LogoImg } from '../../../asset/crown.svg';
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-Container" to="/">
+          <LogoImg />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            Shop
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
